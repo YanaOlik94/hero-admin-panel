9bc1c549b9f918123159ba9323e604c6a153c699
Yana Olik <yanaolik94@gmail.com>
2025-06-27 14:24:34 +0300

    init commit

---
+++ src/actions/index.js
@@ -0,0 +1,63 @@
+// загрузка героїв
+export const heroesFetching = () => {
+  return {
+    type: 'HEROES_FETCHING',
+  };
+};
+
+export const heroesFetched = (heroes) => {
+  return {
+    type: 'HEROES_FETCHED',
+    payload: heroes,
+  };
+};
+
+export const heroesFetchingError = () => {
+  return {
+    type: 'HEROES_FETCHING_ERROR',
+  };
+};
+
+// загрузка філтрів
+export const filtersFetching = () => {
+  return {
+    type: 'FILTERS_FETCHING',
+  };
+};
+
+export const filtersFetched = (filters) => {
+  return {
+    type: 'FILTERS_FETCHED',
+    payload: filters,
+  };
+};
+
+export const filtersFetchingError = () => {
+  return {
+    type: 'FILTERS_FETCHING_ERROR',
+  };
+};
+// зміна філтрів
+export const activeFilterChanged = (filter) => {
+  return {
+    type: 'ACTIVE_FILTER_CHANGED',
+    payload: filter,
+  };
+};
+
+// створення героя
+export const heroCreated = (hero) => {
+  return {
+    type: 'HERO_CREATED',
+    payload: hero,
+  };
+};
+
+
+// видалення героя
+export const heroDeleted = (id) => {
+  return {
+    type: 'HERO_DELETED',
+    payload: id,
+  };
+};

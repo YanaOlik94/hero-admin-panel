daae90136f8e156d4153dfb2d50e1065ff2cc0c7
Yana Olik <yanaolik94@gmail.com>
2025-07-30 16:43:37 +0300

    added reselect and combineReducers

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,6 +2,7 @@ import { useHttp } from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { createSelector } from 'reselect';
 
 import {
   heroesFetching,
@@ -16,7 +17,28 @@ import './heroesList.scss';
 
 
 const HeroesList = () => {
-  const { filteredHeroes, heroesLoadingStatus } = useSelector((state) => state);
+  const filteredHeroesSelector = createSelector( //  creating memoized "selector" function, check if value was changed
+    (state) => state.filters.activeFilter,
+    (state) => state.heroes.heroes,
+    (filter, heroes) => {
+      if (filter === 'all') {
+        return heroes;
+      } else {
+        return heroes.filter((item) => item.element === filter);
+      }
+    }
+  );
+
+  // const filteredHeroes =  useSelector(state => {
+  //    if (state.filters.activeFilter === 'all') {
+  //     return state.heroes.heroes;
+  //    } else {
+  //     return state.heroes.heroes.filter((item) => item.element === state.filters.activeFilter);
+  //    }
+  // })
+
+  const filteredHeroes = useSelector(filteredHeroesSelector);
+  const heroesLoadingStatus = useSelector((state) => state.heroesLoadingStatus);
   const dispatch = useDispatch();
   const { request } = useHttp();
 
@@ -29,16 +51,14 @@ const HeroesList = () => {
     // eslint-disable-next-line
   }, []);
 
-
   const onDelete = useCallback(
     (id) => {
-     
       request(`http://localhost:3001/heroes/${id}`, 'DELETE')
         .then((data) => console.log(data, 'Deleted'))
         .then(dispatch(heroDeleted(id)))
         .catch((err) => console.log(err));
-      // eslint-disable-next-line
     },
+    // eslint-disable-next-line
     [request]
   );
 
